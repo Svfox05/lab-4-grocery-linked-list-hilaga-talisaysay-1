ade7a76350f6dbce35d0e182deaa8402bb426ee6
Silas Vincient E. Talisaysay <svtalisaysay@yahoo.com>
2017-09-22 20:32:40 +0800

    Update GroceryListLinked.java

--- GroceryListLinked.java
+++ GroceryListLinked.java
@@ -4,21 +4,54 @@ class GroceryListLinked implements IGroceryList {
   int size = 0;
   
   GroceryListLinked() {}
-  
+    
   public boolean add(GroceryItem item) {
-    return false;
+    GroceryNode newNode = new GroceryNode(item, head);
+    head = newNode;
+    return true;
   }
   
   public boolean remove(String name) {
+    GroceryNode current = head;
+    GroceryNode previous = current;
+    if (current == null){
+      return false;
+    }
+    else {
+      while (current != null){
+        if (name.equals(current.data.name)){
+          previous.next = current.next;
+          current.next = null;
+          
+          return true;
+        }
+        previous = current;
+        current = current.next;
+      }
+    }
     return false;
   }
   
-  public boolean markAsBought(String name) {
+public boolean markAsBought(String name) {
+    GroceryNode current = head;
+    
+    while (current != null) {
+      if (current.data.equals(new GroceryItem(name, 0))) {
+        current.data.isBoughtTrue();
+        return true;
+      }
+      current = current.next;
+    }
     return false;
   }
   
   public void display() {
-    
+    GroceryNode current = head;
+
+    while(current != null){
+      System.out.println(current.data);
+      current = current.next;
+    }
   }
 }
 
